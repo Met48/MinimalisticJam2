c91815f3119599adcc03ccb4ec2270d2fc39632d
Anthony <anthony.huazhang@gmail.com>
2017-12-27 14:33:09 -0500

    Basic controls, rigidbody, colliders.

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    float speed = 20f;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var input = Input.GetAxis("Horizontal");
+        var movement = input * speed;
+
+        rb.velocity = new Vector3(movement, rb.velocity.y, 0);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(new Vector3(0, 600, 0));
+        }
+	}
+}
