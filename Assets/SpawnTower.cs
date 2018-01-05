cf4046d7182ae5e3695f352cc0cc7f593d1efaed
Anthony <anthony.huazhang@gmail.com>
2018-01-04 21:15:07 -0500

    Tower spawns (lmb click) after silhouette activation (using the 1 button). TODO: Make tower silhouette collide with ground.

--- Assets/SpawnTower.cs
+++ Assets/SpawnTower.cs
@@ -4,17 +4,61 @@ using UnityEngine;
 
 public class SpawnTower : MonoBehaviour {
 
-    public GameObject target;
-    float floorPos = -2.9f;
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetButtonDown("Fire1"))
-        {
-            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
-            
-            // cannot collide with player
-            // if colliding with ground
-            Instantiate(target, new Vector3(p.x, floorPos, 0.0f), Quaternion.identity);
+    public GameObject tower;
+    public GameObject invalidTower;
+    public GameObject player;
+    // float floorPos = -2.9f;
+    bool towerSilhouetteVisible;
+    Collision2D col;
+    Vector3 p;
+
+    private void Start()
+    {
+        invalidTower = GameObject.Find("InvalidTower");
+        player = GameObject.Find("Player");
+
+        invalidTower.SetActive(false);
+        towerSilhouetteVisible = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        Physics2D.IgnoreCollision(player.GetComponent<BoxCollider2D>(), invalidTower.GetComponent<BoxCollider2D>());
+
+        if (Input.anyKeyDown)
+            ShowTowerSilhouette();
+
+        if (towerSilhouetteVisible == false)
+            invalidTower.SetActive(false);
+
+        invalidTower.transform.position = p;
+    }
+
+    private void ShowTowerSilhouette()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            towerSilhouetteVisible = true;
+            invalidTower.SetActive(true);
         }
-	}
+
+        if (towerSilhouetteVisible == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                towerSilhouetteVisible = false;
+                SpawnTheTower();
+            }
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                towerSilhouetteVisible = false;
+            }
+        }
+    }
+
+    private void SpawnTheTower()
+    {
+        Instantiate(tower, new Vector3(p.x, p.y, 0.0f), Quaternion.identity);
+        invalidTower.SetActive(false);
+    }
 }
