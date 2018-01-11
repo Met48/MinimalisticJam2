8413f314e820eb67f10b191c2bb454a00632c7db
zhanga <anthony.huazhang@gmail.com>
2018-01-11 02:11:03 -0500

    Snappy tower spawns with surface detection functional. TODO: Make different kinds of towers, and make the towers do stuff.

--- Assets/SpawnTower.cs
+++ Assets/SpawnTower.cs
@@ -8,79 +8,118 @@ public class SpawnTower : MonoBehaviour {
     public GameObject invalidTower;
     public GameObject player;
     public GameObject tile;
-    float floorPos;
-    bool updateYAxis;
-    bool towerSilhouetteVisible;
-    Vector3 p;
+    public GameObject validTower;
+    float floorPosY;
+    float invalidTowerColliderY;
+    float floorTransformPos;
+    bool canPlace;
+    bool invalidTowerSilhouetteVisible;
+    RaycastHit2D detectFloor;
+    Vector2 detectFloorVector;
+    Vector3 silhouettePos;
+    Vector3 mousePos;
 
     private void Start()
     {
         invalidTower.SetActive(false);
-        updateYAxis = true;
-        towerSilhouetteVisible = false;
+        validTower.SetActive(false);
+        canPlace = false;
+        invalidTowerSilhouetteVisible = false;
     }
 
     // Update is called once per frame
     void Update () {
+        // To ignore player collision with invalidTower.
         Physics2D.IgnoreCollision(player.GetComponent<BoxCollider2D>(), invalidTower.GetComponent<BoxCollider2D>());
-        p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, floorPos, 10.0f));
-        invalidTower.transform.position = p;
+        Physics2D.IgnoreCollision(player.GetComponent<BoxCollider2D>(), invalidTower.GetComponent<BoxCollider2D>());
+        // To convert the mouse position relative to camera to Unity's transform matrix.
+        silhouettePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        // To turn the invalidTower's GameObject position to the mouse position.
+        invalidTower.transform.position = silhouettePos;
+        validTower.transform.position = silhouettePos;
+        // To find the y value of the bottom of the invalidTower's BoxCollider2D.
+        invalidTowerColliderY = invalidTower.transform.position.y - (invalidTower.GetComponent<BoxCollider2D>().size.y / 2);
+        // To delegate the location of the bottom of the box collider to a Vector2.
+        detectFloorVector = new Vector2(invalidTower.transform.position.x, invalidTowerColliderY);
+        detectFloor = Physics2D.Raycast(detectFloorVector, -Vector2.up, 10f);
+        // To find the exact location of the surface beneath the object.
         UpdateY();
+        invalidTower.transform.position = silhouettePos;
+        validTower.transform.position = silhouettePos;
 
         if (Input.anyKeyDown)
-            ShowTowerSilhouette();
+            ShowInvalidTowerSilhouette();
 
-        if (towerSilhouetteVisible == false)
+        if (invalidTowerSilhouetteVisible == false)
             invalidTower.SetActive(false);
+
+        if (canPlace == true)
+        {
+            validTower.SetActive(true);
+        }
+
+        else
+        {
+            validTower.SetActive(false);
+        }
     }
 
     private void UpdateY()
     {
-        if (updateYAxis == true)
+        if (invalidTowerColliderY != invalidTowerColliderY - detectFloor.distance)
+            floorTransformPos = invalidTowerColliderY - detectFloor.distance;
+
+        if ((floorTransformPos >= invalidTowerColliderY || floorTransformPos < (invalidTowerColliderY + 2)) && mousePos.y < floorTransformPos + 4f)
         {
-            if (invalidTower.GetComponent<BoxCollider2D>().IsTouching(tile.GetComponent<Collider2D>()))
-            {
-                floorPos = Input.mousePosition.y;
-                updateYAxis = false;
-            }
-            else
-            {
-                floorPos = Input.mousePosition.y;
-            }
+            silhouettePos.y = floorTransformPos + (invalidTower.GetComponent<BoxCollider2D>().size.y / 2);
+            canPlace = true;
         }
-        else if (updateYAxis == false)
-        {
-            if (Input.mousePosition.y >= floorPos + 50)
-            {
-                updateYAxis = true;
-                floorPos = Input.mousePosition.y;
-            }
-        }
+
+        else
+            canPlace = false;
+            
     }
-    private void ShowTowerSilhouette()
+
+    private void ShowInvalidTowerSilhouette()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            towerSilhouetteVisible = true;
+            invalidTowerSilhouetteVisible = true;
             invalidTower.SetActive(true);
         }
 
-        if (towerSilhouetteVisible == true)
+        if (invalidTowerSilhouetteVisible == true)
         {
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                towerSilhouetteVisible = false;
-                SpawnTheTower();
+            if (canPlace == true)
+            { 
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    invalidTowerSilhouetteVisible = false;
+                    canPlace = false;
+                    SpawnTheTower();
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse1)) { 
+                    invalidTowerSilhouetteVisible = false;
+                    canPlace = false;
+                }
             }
-            if (Input.GetKeyDown(KeyCode.Mouse1))
+            else
             {
-                towerSilhouetteVisible = false;
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    invalidTowerSilhouetteVisible = false;
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse1))
+                {
+                    invalidTowerSilhouetteVisible = false;
+                }
             }
         }
     }
 
     private void SpawnTheTower()
     {
-        Instantiate(tower, new Vector3(p.x, p.y, 0.0f), Quaternion.identity);
+        Instantiate(tower, new Vector3(silhouettePos.x, silhouettePos.y, 0.0f), Quaternion.identity);
         invalidTower.SetActive(false);
     }
 }
